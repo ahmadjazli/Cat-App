bfbd38a5836dc5c46de3a248192f1ffd39a21165
Ahmad Jazli Johari <AJ00664688@techmahindra.com>
2021-11-21 15:07:55 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
-function App() {
+
+const App = () => {
+  const [cats, setCatState] = useState(null);
+
+    const getRequest = async () => {    
+    const url = `https://api.thecatapi.com/v1/breeds`;
+		// const url = `https://api.thecatapi.com/v1/images/search?breed_ids=beng&api_key=4e2b4a7f-8b0c-474e-84c1-cf1db8af301a`;
+
+		const response = await fetch(url);
+		const responseJson = await response.json();
+    
+    setCatState(responseJson);
+	};
+  
+  console.log(cats);
+
+  useEffect (() => {
+    getRequest();
+   },[]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className='container-fluid cat-app'>
+			<div className='row d-flex align-items-center mt-4 mb-4'>
+        Test
+			</div>
+		</div>
   );
 }
 
